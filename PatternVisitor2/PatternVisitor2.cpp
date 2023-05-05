3bc045d577ad7375647802ffb5fc5f7cd6b41ca7
Katya Zhukova <gjugjuka2002@gmail.com>
2023-05-05 21:44:59 +0400

    Иерархия классов «Занятие» («Лекция», «Лабораторное занятие», «Семинар»).

---
+++ PatternVisitor2/PatternVisitor2.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <vector>
+
+// Объявление трех классов(предварительное описание): Лекция, Лабораторное занятие, Семинар
+class Lecture;
+class Lab;
+class Seminar;
+
+// Абстрактный класс Занятие
+class Lesson {// базовый класс-элемент
+public:
+    virtual void accept(class Visitor& v) = 0;
+    virtual ~Lesson() {}//виртуальный деструктор
+};
+
+// Класс Лекция
+class Lecture : public Lesson {//конкретный класс-элемент
+public:
+    Lecture(int hours, int max_score) : hours_(hours), max_score_(max_score) {}//конструктор
+    virtual ~Lecture() {}//виртуальный декструктор
+    // Переопределенный метод accept, принимающий объект Visitor по ссылке и не возвращающий никакого значения
+    // Этот метод будет вызываться у Visitor, передавая в него this (текущий объект Lecture)
+    void accept(Visitor& v) override;
+    int hours_;// количество часов
+    int max_score_;// максимальное количество баллов
+};
+
+// Класс Лабораторное занятие
+class Lab : public Lesson {
+public:
+    Lab(int hours, int max_score) : hours_(hours), max_score_(max_score) {}
+    virtual ~Lab() {}
+    void accept(Visitor& v) override;
+    int hours_;
+    int max_score_;
+};
+
+// Класс Семинар
+class Seminar : public Lesson {
+public:
+    Seminar(int hours, int max_score) : hours_(hours), max_score_(max_score) {}
+    virtual ~Seminar() {}
+    void accept(Visitor& v) override;
+    int hours_;
+    int max_score_;
+};
+
+
+// Абстрактный класс Visitor
+class Visitor {
+public:
+    // Виртуальная функция visit для посещения лекции,
+    // которая должна быть реализована в производных классах
+    virtual void visit(Lecture& lecture) = 0;
+    // Виртуальная функция visit для посещения лабораторной работы,
+    // которая должна быть реализована в производных классах
+    virtual void visit(Lab& lab) = 0;
+    // Виртуальная функция visit для посещения семинара,
+    // которая должна быть реализована в производных классах
+    virtual void visit(Seminar& seminar) = 0;
+};
+
+// Класс Предмет
+class Subject {
+public:
+    // конструктор класса, который принимает ссылку на вектор указателей на объекты типа Lesson и инициализирует соответствующий член класса lessons_
+    Subject(std::vector<Lesson*>& lessons) : lessons_(lessons) {}
+    void accept(Visitor& v) {// объявление публичного метода accept, который принимает объект типа Visitor по ссылке
+        for (auto lesson : lessons_) {
+            lesson->accept(v);//вызов метода accept для текущего объекта типа Lesson, передавая ему объект типа Visitor
+        }
+    }
+private:
+    std::vector<Lesson*> lessons_;// вектор указателей на объекты типа Lesson, хранящийся в классе
+};
+
+// Класс HourCounterVisitor
+class HourCounterVisitor : public Visitor {
+public:
+    // Определение публичных переменных, которые будут содержать суммарное количество часов для каждого типа урока
+    int total_lecture_hours;
+    int total_lab_hours;
+    int total_seminar_hours;
+
+    HourCounterVisitor() : total_lecture_hours(0), total_lab_hours(0), total_seminar_hours(0) {}//конструктор
+    // Определение функции visit, которая принимает объект класса Lecture и переопределяет чисто виртуальную функцию из базового класса Visitor
+    void visit(Lecture& lecture) override { total_lecture_hours += lecture.hours_; }// Увеличение значения переменной на количество часов в лекции
+    void visit(Lab& lab) override { total_lab_hours += lab.hours_; }
+    void visit(Seminar& seminar) override { total_seminar_hours += seminar.hours_; }
+};
+
+// Класс ScoreCounterVisitor
+class ScoreCounterVisitor : public Visitor {
+public:
+    int lecture_max_score_;
+    int lab_max_score_;
+    int seminar_max_score_;
+    ScoreCounterVisitor() : lecture_max_score_(0), lab_max_score_(0), seminar_max_score_(0) {}
+    void visit(Lecture& lecture) override {
+        if (lecture.max_score_ > lecture_max_score_) {
+            lecture_max_score_ = lecture.max_score_;
+        }
+    }
+    void visit(Lab& lab) override {
+        if (lab.max_score_ > lab_max_score_) {
+            lab_max_score_ = lab.max_score_;
+        }
+    }
+    void visit(Seminar& seminar) override {
+        if (seminar.max_score_ > seminar_max_score_) {
+            seminar_max_score_ = seminar.max_score_;
+        }
+    }
+};
+
+//реализация метода accept
+//Это определение функции-члена класса Lecture, которая принимает объект класса-посетителя
+//Visitor по ссылке и вызывает функцию visit() этого посетителя, передавая ей объект Lecture по ссылке
+void Lecture::accept(Visitor& v) {
+    v.visit(*this);
+}
+
+void Lab::accept(Visitor& v) {
+    v.visit(*this);
+}
+
+void Seminar::accept(Visitor& v) {
+    v.visit(*this);
+}
+
+// Пример использования
+int main() {
+    std::vector<Lesson*> lessons = {
+        new Lecture(2, 100),
+        new Lecture(5, 120),
+        new Lab(3, 50),
+        new Lab(6, 30),
+        new Seminar(1, 75),
+        new Seminar(2, 120)
+    };
+    // Создаем объект предмета
+    Subject subject(lessons);
+
+    // Создаем объекты Visitor
+    HourCounterVisitor hour_counter;
+    ScoreCounterVisitor score_counter;
+
+    subject.accept(hour_counter);// Принимаем Visitor для подсчета количества часов
+    subject.accept(score_counter);// Принимаем Visitor для нахождения максимального балла
+
+    std::cout << "Lecture hours: " << hour_counter.total_lecture_hours << "\n";
+    std::cout << "Lab hours: " << hour_counter.total_lab_hours << "\n";
+    std::cout << "Seminar hours: " << hour_counter.total_seminar_hours << "\n";
+    std::cout << "Total hours: " << hour_counter.total_lecture_hours + hour_counter.total_lab_hours + hour_counter.total_seminar_hours << "\n";
+    std::cout << "Lecture max score: " << score_counter.lecture_max_score_ << "\n";
+    std::cout << "Lab max score: " << score_counter.lab_max_score_ << "\n";
+    std::cout << "Seminar max score: " << score_counter.seminar_max_score_ << "\n";
+    std::cout << "Total max score: " << score_counter.lecture_max_score_ + score_counter.lab_max_score_ + score_counter.seminar_max_score_ << "\n";
+
+    // Освобождение памяти
+    for (auto lesson : lessons) {
+        delete lesson;
+    }
+
+    return 0;
+}
---
+++ TestForVisitor2/TestForVisitor2.cpp
@@ -0,0 +1,341 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include "D:\PatternVisitor\6_2\6_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LessonTests
+{
+
+    // Test for Lecture
+    TEST_CLASS(TestsForLecture)
+    {
+    public:
+        //Тест для конструктора класса Лекция
+        TEST_METHOD(LectureConstructorTestsSetsHoursAndMaxScore)
+        {
+            int hours = 3;
+            int max_score = 100;
+
+            Lecture lecture(hours, max_score);
+
+            Assert::AreEqual(hours, lecture.hours_);
+            Assert::AreEqual(max_score, lecture.max_score_);
+        }
+
+        TEST_METHOD(TestHourCounterVisitorforLecture)
+        {
+            // Создаем объект HourCounterVisitor
+            HourCounterVisitor visitor;
+
+            // Создаем набор занятий "Лекция"
+            std::vector<Lesson*> lectures;
+            lectures.push_back(new Lecture(2, 100));
+            lectures.push_back(new Lecture(6, 200));
+            lectures.push_back(new Lecture(12, 110));
+            lectures.push_back(new Lecture(10, 30));
+            lectures.push_back(new Lecture(8, 220));
+            Subject subject(lectures);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта HourCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что общее количество часов для типа урока "Лекция" было посчитано верно
+            Assert::AreEqual(38, visitor.total_lecture_hours);
+        }
+
+        TEST_METHOD(TestScoreCounterVisitorLecture)
+        {
+            // Создаем объект ScoreCounterVisitor
+            ScoreCounterVisitor visitor;
+
+            // Создаем набор занятий "Lectures"
+            std::vector<Lesson*> lectures;
+            lectures.push_back(new Lecture(2, 100));
+            lectures.push_back(new Lecture(6, 200));
+            Subject subject(lectures);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта ScoreCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что максимальное количество баллов для типа урока "Lectures" было посчитано верно
+            Assert::AreEqual(200, visitor.lecture_max_score_);
+        }
+
+        TEST_METHOD(TestLectureAccept)
+        {
+            Lecture lecture(10, 100);
+            HourCounterVisitor visitor;
+            lecture.accept(visitor);
+
+            Assert::AreEqual(visitor.total_lecture_hours, 10);
+            Assert::AreEqual(visitor.total_lab_hours, 0);
+            Assert::AreEqual(visitor.total_seminar_hours, 0);
+        }
+    };
+
+    // Test for Lab
+    TEST_CLASS(TestsForLab)
+    {
+    public:
+        TEST_METHOD(LabConstructorTestsSetsHoursAndMaxScore)
+        {
+            int hours = 2;
+            int max_score = 50;
+
+            Lab lab(hours, max_score);
+
+            Assert::AreEqual(hours, lab.hours_);
+            Assert::AreEqual(max_score, lab.max_score_);
+        }
+
+        TEST_METHOD(TestHourCounterVisitorforLab)
+        {
+            // Создаем объект HourCounterVisitor
+            HourCounterVisitor visitor;
+
+            // Создаем набор занятий "Лабораторная работа"
+            std::vector<Lesson*> labs;
+            labs.push_back(new Lab(3, 50));
+            labs.push_back(new Lab(4, 100));
+            labs.push_back(new Lab(2, 110));
+            Subject subject(labs);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта HourCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что общее количество часов для типа урока "Лабораторная работа" было посчитано верно
+            Assert::AreEqual(9, visitor.total_lab_hours);
+        }
+
+        TEST_METHOD(TestScoreCounterVisitorLab)
+        {
+            // Создаем объект ScoreCounterVisitor
+            ScoreCounterVisitor visitor;
+
+            // Создаем набор занятий "Lab"
+            std::vector<Lesson*> lab;
+            lab.push_back(new Lab(5, 150));
+            lab.push_back(new Lab(3, 50));
+            lab.push_back(new Lab(4, 100));
+            Subject subject(lab);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта ScoreCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что максимальное количество баллов для типа урока "Lab" было посчитано верно
+
+            Assert::AreEqual(150, visitor.lab_max_score_);
+        }
+
+        TEST_METHOD(TestLabAccept)
+        {
+            Lab lab(5, 50);
+            HourCounterVisitor visitor;
+            lab.accept(visitor);
+
+            Assert::AreEqual(visitor.total_lecture_hours, 0);
+            Assert::AreEqual(visitor.total_lab_hours, 5);
+            Assert::AreEqual(visitor.total_seminar_hours, 0);
+        }
+    };
+
+    // Test constructor for Seminar
+    TEST_CLASS(TestsForSeminar)
+    {
+    public:
+        TEST_METHOD(SeminarConstructorTestsSetsHoursAndMaxScore)
+        {
+
+            int hours = 1;
+            int max_score = 25;
+
+            Seminar seminar(hours, max_score);
+
+            Assert::AreEqual(hours, seminar.hours_);
+            Assert::AreEqual(max_score, seminar.max_score_);
+        }
+
+        TEST_METHOD(TestHourCounterVisitorforSeminar)
+        {
+            // Создаем объект HourCounterVisitor
+            HourCounterVisitor visitor;
+
+            // Создаем набор занятий "Семинар"
+            std::vector<Lesson*> seminars;
+            seminars.push_back(new Seminar(1, 75));
+            seminars.push_back(new Seminar(2, 100));
+            seminars.push_back(new Seminar(5, 120));
+            Subject subject(seminars);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта HourCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что общее количество часов для типа урока "Семинар" было посчитано верно
+            Assert::AreEqual(8, visitor.total_seminar_hours);
+        }
+
+        TEST_METHOD(TestScoreCounterVisitorSeminar)
+        {
+            // Создаем объект ScoreCounterVisitor
+            ScoreCounterVisitor visitor;
+
+            // Создаем набор занятий "Seminar"
+            std::vector<Lesson*> lessons;
+            lessons.push_back(new Seminar(1, 75));
+            lessons.push_back(new Seminar(2, 100));
+            Subject subject(lessons);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта ScoreCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что максимальное количество баллов для типа урока "Seminar" было посчитано верно
+
+            Assert::AreEqual(100, visitor.seminar_max_score_);
+        }
+
+        TEST_METHOD(TestSeminarAccept)
+        {
+            Seminar seminar(2, 20);
+            HourCounterVisitor visitor;
+            seminar.accept(visitor);
+
+            Assert::AreEqual(visitor.total_lecture_hours, 0);
+            Assert::AreEqual(visitor.total_lab_hours, 0);
+            Assert::AreEqual(visitor.total_seminar_hours, 2);
+        }
+
+    };
+
+    TEST_CLASS(TestsForAllTypes)
+    {
+    public:
+        //общее количество часов для каждого типа
+        TEST_METHOD(TestHourCounterVisitorForAllTypes)
+        {
+            // Создаем объект HourCounterVisitor
+            HourCounterVisitor visitor;
+
+            // Создаем набор занятий
+            std::vector<Lesson*> lessons;
+            lessons.push_back(new Lecture(2, 100));
+            lessons.push_back(new Lecture(3, 150));
+            lessons.push_back(new Lecture(12, 120));
+            lessons.push_back(new Lab(3, 50));
+            lessons.push_back(new Seminar(1, 75));
+            lessons.push_back(new Seminar(2, 185));
+            Subject subject(lessons);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта HourCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что общее количество часов для каждого типа урока было посчитано верно
+            Assert::AreEqual(17, visitor.total_lecture_hours);
+            Assert::AreEqual(3, visitor.total_lab_hours);
+            Assert::AreEqual(3, visitor.total_seminar_hours);
+        }
+        //общее количество баллов для каждого типа
+        TEST_METHOD(TestScoreCounterVisitorForAllTypes)
+        {
+            // Создаем объект ScoreCounterVisitor
+            ScoreCounterVisitor visitor;
+
+            // Создаем набор занятий
+            std::vector<Lesson*> lessons;
+            lessons.push_back(new Lecture(2, 100));
+            lessons.push_back(new Lecture(6, 200));
+            lessons.push_back(new Lab(3, 50));
+            lessons.push_back(new Lab(4, 100));
+            lessons.push_back(new Seminar(1, 75));
+            lessons.push_back(new Seminar(2, 100));
+            Subject subject(lessons);
+
+            // Вызываем метод accept у объекта Subject с передачей ему объекта ScoreCounterVisitor
+            subject.accept(visitor);
+
+            // Проверяем, что максимальное количество баллов для каждого типа урока было посчитано верно
+            Assert::AreEqual(200, visitor.lecture_max_score_);
+            Assert::AreEqual(100, visitor.lab_max_score_);
+            Assert::AreEqual(100, visitor.seminar_max_score_);
+        }
+        //тут все вместе, и кол-во баллов и кол-во часов
+        TEST_METHOD(TestSubjectAccept)
+        {
+            std::vector<Lesson*> lessons;
+            lessons.push_back(new Lecture(10, 100));
+            lessons.push_back(new Lecture(20, 150));
+            lessons.push_back(new Lab(5, 50));
+            lessons.push_back(new Seminar(2, 20));
+            lessons.push_back(new Seminar(3, 18));
+
+            Subject subject(lessons);
+            HourCounterVisitor hour_visitor;
+            ScoreCounterVisitor score_visitor;
+
+            subject.accept(hour_visitor);
+            subject.accept(score_visitor);
+
+            Assert::AreEqual(hour_visitor.total_lecture_hours, 30);
+            Assert::AreEqual(hour_visitor.total_lab_hours, 5);
+            Assert::AreEqual(hour_visitor.total_seminar_hours, 5);
+
+            Assert::AreEqual(score_visitor.lecture_max_score_, 150);
+            Assert::AreEqual(score_visitor.lab_max_score_, 50);
+            Assert::AreEqual(score_visitor.seminar_max_score_, 20);
+
+            for (auto lesson : lessons) {
+                delete lesson;
+            }
+        }
+        //аналогичное было выше, это просто другая запись
+        TEST_METHOD(TestHourCounterVisitor)
+        {
+            std::vector<Lesson*> lessons = { new Lecture(2, 100), new Lab(3, 90), new Seminar(1, 80), new Lecture(3, 100), new Seminar(10, 80) };
+            Subject subject(lessons);
+            HourCounterVisitor visitor;
+            subject.accept(visitor);
+
+            Assert::AreEqual(5, visitor.total_lecture_hours);
+            Assert::AreEqual(3, visitor.total_lab_hours);
+            Assert::AreEqual(11, visitor.total_seminar_hours);
+        }
+        //аналогичное было выше, это просто другая запись
+        TEST_METHOD(TestScoreCounterVisitor)
+        {
+            std::vector<Lesson*> lessons = { new Lecture(2, 100), new Lab(3, 90), new Seminar(1, 80) };
+            Subject subject(lessons);
+            ScoreCounterVisitor visitor;
+            subject.accept(visitor);
+
+            Assert::AreEqual(100, visitor.lecture_max_score_);
+            Assert::AreEqual(90, visitor.lab_max_score_);
+            Assert::AreEqual(80, visitor.seminar_max_score_);
+        }
+
+        TEST_METHOD(TestHourCounterVisitorEmptySubject)
+        {
+            std::vector<Lesson*> lessons;
+            Subject subject(lessons);
+            HourCounterVisitor visitor;
+            subject.accept(visitor);
+
+            Assert::AreEqual(0, visitor.total_lecture_hours);
+            Assert::AreEqual(0, visitor.total_lab_hours);
+            Assert::AreEqual(0, visitor.total_seminar_hours);
+        }
+
+        TEST_METHOD(TestScoreCounterVisitorEmptySubject)
+        {
+            std::vector<Lesson*> lessons;
+            Subject subject(lessons);
+            ScoreCounterVisitor visitor;
+            subject.accept(visitor);
+
+            Assert::AreEqual(0, visitor.lecture_max_score_);
+            Assert::AreEqual(0, visitor.lab_max_score_);
+            Assert::AreEqual(0, visitor.seminar_max_score_);
+        }
+
+    };
+}
